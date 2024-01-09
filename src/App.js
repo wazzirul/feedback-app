33c5f12a042126c3074d255c5d9f204ccecca15d
wzrlazzan <amwazirul@gmail.com>
2024-01-09 11:36:42 +0700

    feat : first init and explanation of react front to back React Basics & JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id:1, text: 'comment 1'},
+    {id:2, text: 'comment 2'},
+    {id:3, text: 'comment 3'},
+  ]
+
+  const loading = false
+  const showComments = true
+
+  if(loading) return <p>Loading...</p>
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {showComments && commentBlock}
+    </div>
+  )
 }
 
-export default App;
+export default App
