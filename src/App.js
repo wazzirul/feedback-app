364c614d5df2d15ec63a747065574ca9eb91611e
wzrlazzan <amwazirul@gmail.com>
2024-01-14 09:06:54 +0700

    feat : completing all 3. Components, Props & State

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
+import { useState } from "react"
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    {id:1, text: 'comment 1'},
-    {id:2, text: 'comment 2'},
-    {id:3, text: 'comment 3'},
-  ]
+  const [feedback, setFeedback] = useState(FeedbackData)
 
-  const loading = false
-  const showComments = true
-
-  if(loading) return <p>Loading...</p>
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
 
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+      </div>
+    </>
   )
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+
+const FeedbackData= [
+  {
+    id: 1,
+    text: "Lorem ipsum dolor sit amet.",
+    rating: 10
+  },
+  {
+    id: 2,
+    text: "Lorem ipsum dolor sit amet.",
+    rating: 7
+  },
+  {
+    id: 3,
+    text: "Lorem ipsum dolor sit amet.",
+    rating: 9
+  }
+]
+
+export default FeedbackData
