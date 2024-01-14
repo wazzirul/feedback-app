384d424769c7c16ecf256c863507c84a013de90e
wzrlazzan <amwazirul@gmail.com>
2024-01-14 22:31:59 +0700

    feat : 6. Context API, useContext Hook & Deployment -- 3. Moving Functions To Context.mp4

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import {motion, AnimatePresence} from 'framer-motion'
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
 import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const {feedback} = useContext(FeedbackContext)
+
   if(!feedback || feedback.length <= 0){
     return <p>No Feedback Yet</p>
   }
@@ -12,7 +15,7 @@ function FeedbackList({ feedback, handleDelete }) {
       <AnimatePresence>
         {feedback.map((item) => (
           <motion.div key={item.id} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
-            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />            
+            <FeedbackItem key={item.id} item={item} />            
           </motion.div>
         ))}
       </AnimatePresence>
@@ -26,16 +29,6 @@ function FeedbackList({ feedback, handleDelete }) {
   //     ))}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  )
 }
 
 export default FeedbackList
