5bd734922e8bc77df4b2a4b217f807a47c9685c7
wzrlazzan <amwazirul@gmail.com>
2024-01-14 13:57:13 +0700

    feat : 5. Creating Routes & Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from "./pages/AboutPage"
+import AboutLink from "./components/AboutLink"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,14 +24,24 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <Routes>
+          <Route exact path="/" element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+              </>
+            }>
+          </Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutLink />
       </div>
-    </>
+    </Router>
   )
 }
 
